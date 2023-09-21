76000ab1b1d0a7ff6d56429290d5ae759efdd471
Andres Rosales <59161981+AndresRP645@users.noreply.github.com>
2023-09-21 00:02:04 -0600

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database: {
         host: "us-east.connect.psdb.cloud",
-        user: "muheqkgg4kwle1z6nl2c",
-        password: "pscale_pw_Y2VUhN3NYC22WSS6kuh1PZxJtiNF4gpHIaDZMBQA4vi",
+        user: "lft9ucdt8u0j8b8ho02l",
+        password: "pscale_pw_LeZ3UVB9i3Onb27YxKmTttGtLK6XMTefwch3ViCDxWj",
         database: "impactoacademico",
         ssl:{
             rejectUnauthorized: true
